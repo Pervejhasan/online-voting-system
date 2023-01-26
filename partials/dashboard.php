fb091a7563c1c432dc4614dd0b8ce8afb5b10413
Pervejhasan <mdpervej12441@gmail.com>
2023-01-26 17:03:19 +0600

    user profile and group profile design complete

--- partials/dashboard.php
+++ partials/dashboard.php
@@ -11,10 +11,6 @@ if($_SESSION['status']==1){
 ?>
 
 
-
-
-
-
 <!DOCTYPE html>
 <html lang="en">
 
@@ -37,42 +33,91 @@ if($_SESSION['status']==1){
 
         <a href="logout.php" class="float-end"><button class="btn btn-primary text-white">Logout</button></a>
 
-
-
         <h1 class="my-3 text-center">Voting System</h1>
         <hr class=" w-25   m-auto border border-2">
-
+        <!-- =================================================================== -->
         <div class="row my-5">
+            <div class="col-md-2"></div>
+            <div class="col-md-5">
+                <?php
 
-            <div class="col-md-7">
-                <!-- groups -->
+if(isset($_SESSION['groups'])){
+$groups=$_SESSION['groups'];
+for($i=0;$i<count($groups);$i++){
+    ?>
+
                 <div class="row">
                     <div class="col-md-4">
-                        <img src="" alt="group image">
+                        <img src="../uploads/<?php echo $groups[$i]['photo'] ;?> " class="group-img rounded rounded-2"
+                            alt="group image">
                     </div>
                     <div class="col-md-8">
-                        <strong class="text-dark h5">Group name:</strong><br>
-                        <strong class="text-dark h5">Votes:</strong><br>
+                        <strong class="text-dark h5 my-2">Group name: </strong>
+                        <?php echo $groups[$i]['username'] ;?><br>
+                        <strong class="text-dark h5 my-2">Votes: </strong><?php echo $groups[$i]['votes'] ;?><br>
                     </div>
                 </div>
 
+
+                <form action="../actions/voting.php" method="post">
+                    <input type="hidden" name="groupvotes" value="<?php echo $groups[$i]['votes'] ;?>">
+
+                    <input type="hidden" name="groupid" value="<?php echo $groups[$i]['id'] ;?>">
+                    <?php
+
+
+if($_SESSION['status']==1){
+?>
+                    <button class=" btn bg-success my-3 text-white">Voted</button>
+                    <?php
+}
+else{
+    ?>
+                    <button class=" btn bg-success my-3 text-white" type="submit">Vote</button>
+                    <?php  
+}
+?>
+
+                </form>
+                <hr class="w-75 m-auto my-4">
+
+                <?php   
+}
+
+}else{
+?>
+                <div class="container">
+                    <p>No Group to display</p>
+
+                </div>
+
+
+                <?php
+
+}
+?>
+
+
             </div>
 
 
+            <!-- ================================================================= -->
 
-
-
-
-            <div class="col-md-5">
+            <div class="col-md-3 border border border-info rounded-1 rounded d-flex justify-content-center align-items-center flex-column bg-primary"
+                style="height: 250px; width:250px;">
                 <!-- user profile -->
-
-                <img src="../uploads/<?php echo $data['photo'];?>" class="user-img rounded-circle" alt="user image">
+                <img src="../uploads/<?php echo $data['photo'];?>"
+                    class="user-img rounded-circle border border-3 border-white mb-2 my-4" alt="user image">
                 <br>
-                <strong class="text-dark h5">Name: </strong><?php echo $data['username'];?><br><br>
-                <strong class="text-dark h5">Mobile: </strong><?php echo $data['mobile'];?><br><br>
-                <strong class="text-dark h5">Status: </strong><?php echo $status;?><br><br>
+                <p><strong class="text-dark h5">Name: </strong><?php echo $data['username'];?></p>
+                <p><strong class="text-dark h5">Mobile: </strong><?php echo $data['mobile'];?></p>
+                <p><strong class="text-dark h5">Status: </strong><?php echo $status;?></p>
             </div>
-            <hr class="w-75 m-auto">
+
+            <div class="col-md-2"></div>
+
+
+
         </div>
     </div>
 
